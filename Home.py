3fb8e7ee8807e43540ede099b6f408a3b2924f69
Himanshushekhar Chaurasia <162405506+h-chaurasia@users.noreply.github.com>
2026-07-09 15:19:45 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.set_page_config( page_title="CottonExpelOilYieldPred", initial_sidebar_state="expanded", layout="wide")
+st.set_page_config( page_title="CSORPred", initial_sidebar_state="expanded", layout="wide")
 col1, col2 = st.columns([1.5, 20])
 
 with col1:
     st.image("static/images/icarlogo.png", width=150)
 
 with col2:
-    st.markdown("<h1 style='text-align:center;'> CottonExpelOilYieldPred: A Machine Learning-Based Web Resource for Estimation of Cottonseed Oil Yield</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align:center;'> CSORPred: A Machine Learning-Based Web Resource for Cottonseed Oil Recovery Prediction</h1>", unsafe_allow_html=True)
 
 
 st.markdown("---")
@@ -15,13 +15,13 @@ st.text("")
 
 st.header("Welcome to CottonExpelOilYieldPred")
 st.markdown("""
-**CottonExpelOilYieldPred** focuses on developing an easy-to-use portal that helps estimate cottonseed oil yield based on key factors like the moisture content of the cottonseed, the type of expeller used, the screw speed and cake thickness. 
+**CSORPred** focuses on developing an easy-to-use portal that helps estimate cottonseed oil yield based on key factors like the moisture content of the cottonseed, the type of expeller used, the screw speed and cake thickness. 
 
 By entering these details into the system, users can quickly get an idea of how much oil can be produced. 
 
 The portal is designed to support researchers, and oil mill operators in making better decisions and improving the efficiency of the oil extraction process.
 
-CottonExpelOilYieldPred is freely accessible to the academic community at: **https://cottonseed-dss.streamlit.app/**
+CSORPred is freely accessible to the academic community at: **https://csorpred.streamlit.app/**
 """)
 
 st.text("")
